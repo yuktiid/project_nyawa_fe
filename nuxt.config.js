fabb33edb7509d26db327cbf57113dd3d8ea9569
yuktiid <go.yukti.id@gmail.com>
2024-12-30 21:04:53 +0700

    ok7

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,7 +10,14 @@ export default defineNuxtConfig({
       laravel: 'http://127.0.0.1:8000',
       Api_key: '8a224c4dc8ec162d866a8cefd4cc553dced1f3dec2fc71102a69256aa34072d7'
     },
+
   },
+  nitro: {
+    preset: 'node-server',
+    server: {
+      port: 8000, // Ganti dengan port yang Anda inginkan
+    },
+   },
   compatibilityDate: '2024-09-02',
   plugins: [
     '~/plugins/sweetalert2.js', // Menambahkan plugin SweetAlert2
